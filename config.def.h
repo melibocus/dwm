0483049e879be4d20e766a599bad551e5586dc20
melibocus <melibocus@fastmail.fm>
2015-12-16 11:35:52 +0100

    feat: use /opt/scratchpad as path

--- config.def.h
+++ config.def.h
@@ -60,7 +60,7 @@ static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() 
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", normbgcolor, "-nf", normfgcolor, "-sb", selbgcolor, "-sf", selfgcolor, NULL };
 static const char *termcmd[]  = { "st", NULL };
 static const char scratchpadname[] = "itreallywhipsthescratchpadsass";
-static const char *scratchpadcmd[] = { "urxvt", "-title", scratchpadname, "-e", "vim", "~/scratchpad/index.wiki", NULL };
+static const char *scratchpadcmd[] = { "urxvt", "-title", scratchpadname, "-e", "vim", "/opt/scratchpad/index.wiki", NULL };
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
